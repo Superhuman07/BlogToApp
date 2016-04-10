bc9a42ce242db80be8b062014256313539dea6ab
Superhuman07 <bhatiarahul51@gmail.com>
2016-04-10 20:29:31 +0530

    Second Commit

--- app/src/main/java/com/example/admin/blogtoapp/MainActivity.java
+++ app/src/main/java/com/example/admin/blogtoapp/MainActivity.java
@@ -3,12 +3,8 @@ package com.example.admin.blogtoapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-Button button;
-    EditText editText;
 
     private WebView webView;
     @Override
@@ -17,10 +13,12 @@ Button button;
         setContentView(R.layout.activity_main);
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new MyBrowser());
+        // set the previously chosen url
         String url = "http://simplecodeforyou.blogspot.in/";
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.getSettings().setJavaScriptEnabled(true);
         //webView.getScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // load it
         webView.loadUrl(url);
     }
 }
